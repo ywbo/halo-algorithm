201319bead248f15e67c852b4ac1b2585b636550
yuwenbo <yuwenbo00@163.com>
2023-02-28 06:50:55 +0800

    [DESCRIPTION]: [od机试刷题] [MODULE]: HuaweiOD [BRANCH]: master

---
+++ HuaweiOD/src/main/java/com/ilearning/huaweiod/HuaweiOdApplication.java
@@ -0,0 +1,13 @@
+package com.ilearning.huaweiod;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HuaweiOdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HuaweiOdApplication.class, args);
+    }
+
+}
---
+++ HuaweiOD/src/test/java/com/ilearning/huaweiod/HuaweiOdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ilearning.huaweiod;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HuaweiOdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
